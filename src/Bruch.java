7d7f7f3e799ccea7f22061af993848d9ef7d6f5a
Thorsten Reisinger <52202041@edu.campus02.at>
2022-10-31 20:20:37 +0100

    Bruch Erweiterung

--- src/Bruch.java
+++ src/Bruch.java
@@ -9,7 +9,7 @@ public class Bruch {
         this.zaehler = zaehler;
     }
 
-    public double toDecimal(){
+    public double toDecimal() {
         double decimal = 0.0;
 
         decimal = this.zaehler / (this.nenner * 1.0);
@@ -17,11 +17,11 @@ public class Bruch {
         return decimal;
     }
 
-    public void print (){
+    public void print() {
         System.out.println(this.zaehler + "/" + this.nenner);
     }
 
-    public Bruch multiplicate (Bruch b2){
+    public Bruch multiplicate(Bruch b2) {
         int newZaehler = this.zaehler * b2.zaehler;
         int newNenner = this.nenner * b2.nenner;
 
@@ -31,9 +31,50 @@ public class Bruch {
         return b1;
     }
 
-    public Bruch multiplicate (Bruch b2, Bruch b3){
+    public Bruch multiplicate(Bruch b2, Bruch b3) {
         return multiplicate(b2).multiplicate(b3);
     }
+
+    public Bruch multiplicate(Bruch[] arr) {
+        Bruch multipicatedBruch = null;
+
+        /*for (int i = 0; i < arr.length; i++) {
+            if (i == 0){
+                multipicatedBruch = arr[i];
+            }
+            else {
+                multipicatedBruch = multiplicate(arr[i], arr[i-1]);
+            }
+
+        }*/
+
+        if (arr.length >= 2) {
+            multipicatedBruch = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                multipicatedBruch = multipicatedBruch.multiplicate(arr[i]);
+            }
+        }
+
+        return multipicatedBruch;
+    }
+
+    public void trim() {
+        int tempZaehler = this.zaehler,
+                tempNenner = this.nenner,
+                rest;
+
+         while (tempNenner != 0){
+             rest = tempZaehler % tempNenner;
+             tempZaehler = tempNenner;
+             tempNenner = rest;
+         }
+
+        if (tempZaehler > 1) {
+            this.zaehler /= tempZaehler;
+            this.nenner /= tempZaehler;
+        }
+    }
+
 
     public int getNenner() {
         return nenner;
--- src/Bruchrechner.java
+++ src/Bruchrechner.java
@@ -1,8 +1,12 @@
 public class Bruchrechner {
     public static void main(String[] args) {
         
-        Bruch bruch1 = new Bruch(3,5);
-        Bruch bruch2 = new Bruch(6, 7);
+        Bruch bruch1 = new Bruch(3,5),
+         bruch2 = new Bruch(6, 7),
+         bruch3 = new Bruch(5, 17);
+
+
+        Bruch[] arrayBruch = new Bruch[] {bruch1, bruch2, bruch3};
 
         System.out.println("bruch1.toDecimal() = " + bruch1.toDecimal());
         bruch1.print();
@@ -12,5 +16,13 @@ public class Bruchrechner {
 
         Bruch result = bruch1.multiplicate(bruch2);
         result.print();
+
+        Bruch resultArry = new Bruch(0, 0);
+
+        resultArry  = resultArry.multiplicate(arrayBruch);
+        resultArry.print();
+
+        resultArry.trim();
+        resultArry.print();
     }
 }
